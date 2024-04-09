a5082992734e54ef9886923402a32af00c5eae49
Muhammad Ashif Raza <mdashifraza222jj@gmail.com>
2024-04-09 22:22:49 +0530

    meeting title updated

--- app/meeting/[id]/page.tsx
+++ app/meeting/[id]/page.tsx
@@ -2,17 +2,21 @@ import { Metadata } from "next";
 import Meeting from "./Meeting";
 import { currentUser } from "@clerk/nextjs";
 import MeetingLoginPage from "./MeetingLoginPage";
+import { useEffect } from "react";
 
 interface PageProps{
     params:{id:string};
     searchParams:{guest:string}
 }
-export function generateMetaData({params: {id}}:PageProps):Metadata{
-    return {
-        title : `meeting ${id}`
-    }
-}
+export const metadata: Metadata = {
+    title: `Meeting`
+};
+
 export default async function Page({params:{id},searchParams:{guest}}:PageProps) {
+ 
+    metadata.title = `Meeting ${id}`;
+
+    
     const user = await currentUser();
 
     const guestMode = guest === "true"; 
